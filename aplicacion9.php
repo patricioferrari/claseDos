9ed97dc6048358989406ac8aa2d76cb754d43c7a
patricioferrari <ferrari.patricio@yahoo.com.ar>
2016-08-23 20:05:01 -0300

    Guia PHP Parte 1,2 y 3 -  Aplicacion 9 Finalizado (Arrays asociativos)

---
+++ aplicacion9.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>Aplicacion 9 </title>
+</head>
+<body>
+
+	<?php
+
+		$lapicera1 = array('Color' => 'Azul' , 'Marca' => 'Parker' , 'Trazo' => 'Grueso', 'Precio' => '$5');
+		$lapicera2 = array('Color' => 'Verde' , 'Marca' => 'Bic' , 'Trazo' => 'Mediano', 'Precio' => '$6');
+		$lapicera3 = array('Color' => 'Roja' , 'Marca' => 'Uniball' , 'Trazo' => 'Fino', 'Precio' => '$7');
+
+		foreach ($lapicera1 as $dato) 
+		{
+			echo "$dato <br>";
+		}
+		
+		foreach ($lapicera2 as $dato) 
+		{
+			echo "$dato <br>";
+		}
+
+		foreach ($lapicera3 as $dato) 
+		{
+			echo "$dato <br>";
+		}
+
+
+	?>
+</body>
+</html>
